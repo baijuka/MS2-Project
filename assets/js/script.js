ef947939dc3936bb36e3c265602a03308bc57e88
baijuka <baijuka@yahoo.com>
2021-05-19 22:36:58 +0000

    added new elements in html and script added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,65 @@
+var xhr = new XMLHttpRequest();
+var data;
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // document.getElementById("data").innerHTML = JSON.parse(this.responseText);
+       data = JSON.parse(this.responseText);
+        selectCountry(data);
+    }
+};
+
+xhr.open("GET", "https://restcountries.eu/rest/v2/region/europe/");
+
+xhr.send();
+
+function selectCountry(obj) {
+    var select = document.getElementById("countryList"); 
+    for(var i = 0; i < obj.length; i++) {
+    var countryName = obj[i].name;
+    var optionElement = document.createElement("option");
+    optionElement.textContent = countryName;
+    optionElement.value = countryName;
+    select.appendChild(optionElement);
+   }
+  return '';
+    }
+
+/* ------- Populate the array with country names -----------
+var globalData= []; 
+var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', 'https://restcountries.eu/rest/v2/region/europe/');
+  ourRequest.onload = function() {
+    if (ourRequest.status >= 200 && ourRequest.status < 400) {
+       ourData = ourRequest.responseText;
+       globalData  = JSON.parse(ourData);
+      selectCountry(globalData)
+       } else {
+      console.log("We connected to the server, but it returned an error.");
+    }
+    
+  };
+
+  ourRequest.onerror = function() {
+    console.log("Connection error");
+  };
+
+  ourRequest.send();
+
+function selectCountry(options) {
+     var select = document.getElementById("countryList"); 
+     for(var i = 0; i < options.length; i++) {
+    var opt = options[i].name;
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.appendChild(el);
+    }
+   return '';
+     }
+
+     ----------------- */
+
+/*
 var ul = document.getElementById('ul')
 var nextButton = document.getElementById('btnNext');
 var quizbox = document.getElementById('questionBox')
@@ -76,3 +138,41 @@ function button(ele){
 function next(){
     app.next();
     app.allowClick();
+
+    */
+
+/*  country objet Keys & Values ---------------------/
+
+let country = [{"name":"Åland Islands",
+ 	 "topLevelDomain":[".ax"],
+	  "alpha2Code":"AX",
+	 "alpha3Code":"ALA",
+	"callingCodes":["358"],
+	"capital":"Mariehamn",
+	"altSpellings":["AX","Aaland","Aland","Ahvenanmaa"],
+	"region":"Europe",
+	"subregion":"Northern Europe",
+	"population":28875,
+	"latlng":[60.116667,19.9],
+	"demonym":"Ålandish",
+	"area":1580.0,
+	"gini":null,
+	"timezones":["UTC+02:00"],
+	"borders":[],
+	"nativeName":"Åland",
+	"numericCode":"248",
+	"currencies":[{"code":"EUR","name":"Euro","symbol":"€"}],
+	"languages":[{"iso639_1":"sv","iso639_2":"swe","name":"Swedish","nativeName":"svenska"}],
+	"translations":{"de":"Åland","es":"Alandia","fr":"Åland","ja":"オーランド諸島","it":"Isole Aland","br":"Ilhas de Aland","pt":"Ilhas de Aland","nl":"Ålandeilanden","hr":"Ålandski otoci","fa":"جزایر الند"},
+	"flag":"https://restcountries.eu/data/ala.svg",
+	"regionalBlocs":[{"acronym":"EU","name":"European Union","otherAcronyms":[],"otherNames":[]}],
+	"cioc":""},
+
+var countryname = country[0].name;
+var population = country[0].population;
+var area = country[0].area;
+var capital = country[0].capital;
+var currency = country[0].currencies[0].name;
+var laguage = country[0].languages[0].name;
+
+------------------------------ */
