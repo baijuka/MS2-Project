b3a3929a061b509f006c0f6d252cd072c7592325
baijuka <baijuka@yahoo.com>
2021-06-05 11:53:50 +0000

    contry data table population done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var xhr = new XMLHttpRequest();
 var data;
+var userChoice;
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        // document.getElementById("data").innerHTML = JSON.parse(this.responseText);
@@ -23,6 +24,25 @@ function selectCountry(obj) {
    }
   return '';
     }
+
+    function displayData(choice) {
+      let dataReturn = data.filter(cname => cname.name === choice); 
+      document.getElementById("c_name").innerHTML = dataReturn[0].name;
+      document.getElementById("c_cap").innerHTML = dataReturn[0].capital;
+      document.getElementById("c_pop").innerHTML = dataReturn[0].population;
+      document.getElementById("c_area").innerHTML = dataReturn[0].area;
+      document.getElementById("c_currency").innerHTML = dataReturn[0].currencies[0].name;
+      document.getElementById("c_language").innerHTML = dataReturn[0].languages[0].name;
+      document.getElementById("c_region").innerHTML = dataReturn[0].region;
+      document.getElementById("c_dial").innerHTML = dataReturn[0].callingCodes;
+      document.getElementById("c_lat").innerHTML = dataReturn[0].latlng;
+      document.getElementById("c_time").innerHTML = dataReturn[0].timezones;
+      document.getElementById("c_native").innerHTML = dataReturn[0].nativeName;
+      document.getElementById("c_name").innerHTML = dataReturn[0].name;
+      document.getElementById("flag_img").src = dataReturn[0].flag;
+
+     }
+
 
 /* ------- Populate the array with country names -----------
 var globalData= []; 
